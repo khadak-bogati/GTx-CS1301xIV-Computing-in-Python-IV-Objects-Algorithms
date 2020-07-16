c091c1bf6fc02a888cc71a57232ccd60dbcb6f46
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-15 19:55:53 -0500

    CombineClass with case

---
+++ CombineClassCase.py
@@ -0,0 +1,16 @@
+class Person:
+	def __init__(self, name, eyecolor, age):
+		self.name = name
+		self.eyecolor = eyecolor
+		self.age = age
+class Name:
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname
+		self.lastname = lastname
+def capitalizeName(name):
+	name.firstname = name.firstname.upper()
+	name.lastname = name.lastname.upper()
+myPerson = Person(Name("Khadak", "bogati"),"brown", 30)
+capitalizeName(myPerson.name)
+print(myPerson.name.firstname)
+print(myPerson.name.lastname)
